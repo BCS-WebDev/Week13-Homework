cbeaee5b6f94a1c186aeb9199046859ed6c130bd
Kevin Choi <kevin1choi@gmail.com>
2020-08-05 12:56:28 -0700

    Adding offline.js (functional / functional offline)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,12 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/index.js",
+    "/assets/js/index.js",
+    "/assets/js/offline.js",
     "/manifest.webmanifest",
-    "/styles.css",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
+    "/assets/css/styles.css",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
